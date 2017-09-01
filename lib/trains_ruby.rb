1f127c9fda7a3e4c57dfc5ee0419d2cffe82acce
Ardalan Saberi <ardalan.saberi@hotmail.com>
2017-09-01 06:54:15 -0400

    +logging

--- lib/trains_ruby.rb
+++ lib/trains_ruby.rb
@@ -5,7 +5,7 @@ require "trains_ruby/route_distance_query"
 require "trains_ruby/shortest_route_query"
 require "trains_ruby/railroad_helper"
 require "trains_ruby/railroad_system"
-
+require "trains_ruby/tlog"
 
 module TrainsRuby
 
